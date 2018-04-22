f9a8dc26db6db72f174cff373e77843efbddeff9
Joey <jyudel@gmail.com>
2018-04-22 10:54:02 -0400

    changing logic

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
-
+    popup = document;
+      document.body.style.backgroundColor = "red";
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
+      var theText = document.body.innerText
 
     // our logic
     });
