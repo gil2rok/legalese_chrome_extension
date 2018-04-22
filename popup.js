2e9503540466fa4416a2d0e078395bedb104d8a0
Joey <jyudel@gmail.com>
2018-04-22 13:28:43 -0400

    logic and better html

--- popup.js
+++ popup.js
@@ -8,7 +8,8 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
-    var prob = checkProblematicness(plainText);
+    //var prob = checkProblematicness(plainText);
+    var prob = 4;
     if (prob >= 10){document.body.style.backgroundColor = "red";}
     else if (prob >= 3) {document.body.style.backgroundColor = "orange";}
     else if (prob < 3) {document.body.style.backgroundColor = "lime";}
@@ -17,8 +18,22 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 function checkProblematicness(text){
-  return 1;
+  var lines = text.split(/[!.?[\r\n]]+/);
+  var prob_num = 0;
+  var prob_sentences = [];
+  var numOfLines = lines.length;
+  var prob_len = prob_clauses.length;
+  for (var i = 0; i < numOfLines; i++) {
+    lines[i] = lines[i].toLowerCase();
+    for (var j = 0; j < prob_len; j++) {
+      if (lines[i].includes(prob_clauses[j]){
+        prob_num += 1;
+        prob_sentences.push(lines[i]);
+      };
+  };
 };
+return prob_num;
+}
 
 function onWindowLoad() {
   var div = document.getElementById('appendText')
@@ -34,3 +49,11 @@ function onWindowLoad() {
 
 }
 window.onload = onWindowLoad;
+
+prob_clauses = ["disclose to any third party",
+"you may not disclose the results of any benchmark test",
+"revoke the DRM Software's right",
+"revocation lists",
+"automatically downloaded and installed",
+"disabling your ability"
+];
